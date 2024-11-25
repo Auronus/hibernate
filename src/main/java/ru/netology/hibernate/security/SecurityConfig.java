d021d9d4133aaa255887a33795f6b4a9ff0abeb1
Auronus <auronus@mail.ru>
2024-11-25 14:18:00 +0300

    Method Security HW

--- src/main/java/ru/netology/hibernate/security/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate/security/SecurityConfig.java
@@ -1,26 +1,56 @@
 package ru.netology.hibernate.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/persons/by-city").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
-
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().authenticated());
+        http.headers(Customizer.withDefaults());
+        http.sessionManagement(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());
+        http.anonymous(Customizer.withDefaults());
+        http.csrf(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService inMemoryUserDetailsService(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder()
+                .passwordEncoder(passwordEncoder::encode);
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(users.username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN", "READ", "WRITE", "DELETE")
+                .build());
+        userDetailsManager.createUser(users.username("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER", "READ")
+                .build());
+        userDetailsManager.createUser(users.username("writer")
+                .password(passwordEncoder.encode("writer"))
+                .roles("USER", "READ", "WRITE")
+                .build());
+        return userDetailsManager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
