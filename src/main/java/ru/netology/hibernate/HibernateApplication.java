bb6a7a6164a5e0562973447c3b7cff08aef13bdf
Auronus <auronus@mail.ru>
2024-11-25 13:06:15 +0300

    Security HW on spring 6.4.1

--- src/main/java/ru/netology/hibernate/HibernateApplication.java
+++ src/main/java/ru/netology/hibernate/HibernateApplication.java
@@ -1,26 +1,13 @@
 package ru.netology.hibernate;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.netology.hibernate.entity.Persons;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
-public class HibernateApplication implements CommandLineRunner {
-
-
+public class HibernateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HibernateApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
---
+++ src/main/java/ru/netology/hibernate/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package ru.netology.hibernate.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/persons/by-city").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
